4672b3f9b4f4b504fc33d70708b9588752b45efb
Kevin Isom <kevin.isom@gmail.com>
2019-07-11 17:28:03 +1200

    Publisher generated commit [ci skip]

--- src/ui/index.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-import App from './core/app'
-import './css/styles.scss'
-ReactDOM.render(<App />, document.getElementById('root'))
-
-if (process.env.NODE_ENV !== 'production') {
-  module.hot.accept()
-}

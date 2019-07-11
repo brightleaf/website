bb47aa78242e17602347f21b1ae3d7c61c031a54
Kevin Isom <kevin.isom@gmail.com>
2019-07-11 16:39:00 +1200

    webpack

---
+++ src/webpack/prod.config.js
@@ -0,0 +1,61 @@
+const path = require('path')
+const webpack = require('webpack')
+const CopyPlugin = require('copy-webpack-plugin')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+const { BundleAnalyzerPlugin } = require('webpack-bundle-analyzer')
+const baseConfig = require('./config')
+
+const prodConfig = {
+  optimization: {
+    splitChunks: {
+      cacheGroups: {
+        styles: {
+          name: 'styles',
+          test: /\.css$/,
+          chunks: 'all',
+          enforce: true,
+        },
+      },
+    },
+  },
+  plugins: [
+    new MiniCssExtractPlugin({
+      // Options similar to the same options in webpackOptions.output
+      // both options are optional
+      filename: '[name].css',
+      chunkFilename: '[id].css',
+    }),
+    new webpack.DefinePlugin({
+      process: {
+        env: {
+          NODE_ENV: '"production"',
+        },
+      },
+    }),
+    new HtmlWebpackPlugin({
+      title: 'UI Web App',
+      template: './src/ui/index.html',
+    }),
+    new CopyPlugin([
+      {
+        from: path.join(process.cwd(), '/src/public'),
+        to: path.join(process.cwd(), '/dist'),
+      },
+    ]),
+    new BundleAnalyzerPlugin({
+      analyzerMode: 'static',
+      generateStatsFile: true,
+    }),
+  ],
+  output: {
+    path: path.join(process.cwd(), '/dist'),
+    publicPath: '/',
+    filename: 'bundle.js',
+  },
+}
+
+module.exports = {
+  ...baseConfig,
+  ...prodConfig,
+}
